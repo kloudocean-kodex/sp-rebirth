a2ebfb5a67c5a909fadd0c3b44290674e2b535ba
kloudocean-kodex <rajputrupali138@gmail.com>
2026-08-27 18:27:04 +0530

    fix: omit undefined Sanity Studio app id

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -2,6 +2,7 @@ import {defineCliConfig} from 'sanity/cli'
 
 const projectId = process.env.SANITY_STUDIO_PROJECT_ID
 const dataset = process.env.SANITY_STUDIO_DATASET || 'production'
+const appId = process.env.SANITY_STUDIO_APP_ID
 
 export default defineCliConfig({
   api: {
@@ -9,7 +10,7 @@ export default defineCliConfig({
     dataset,
   },
   deployment: {
-    appId: process.env.SANITY_STUDIO_APP_ID,
+    ...(appId ? {appId} : {}),
     autoUpdates: true,
   },
 })
