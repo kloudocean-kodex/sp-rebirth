07d86a437ccddf7b765a48dbd51acb6c19e8ae1c
kloudocean-kodex <rajputrupali138@gmail.com>
2026-08-27 18:26:36 +0530

    fix: type SP_REBIRTH Cloudflare runtime bindings

---
+++ src/cloudflare-env.d.ts
@@ -0,0 +1,10 @@
+declare module 'cloudflare:workers' {
+  interface SPRebirthWorkerEnv {
+    TURNSTILE_SECRET_KEY?: string;
+    LEAD_DELIVERY_WEBHOOK_URL?: string;
+    LEAD_DELIVERY_TOKEN?: string;
+    SANITY_API_READ_TOKEN?: string;
+  }
+
+  export const env: SPRebirthWorkerEnv;
+}
