0041f83978d7a1a2cc741700f12fe25194159401
kloudocean-kodex <rajputrupali138@gmail.com>
2026-08-27 18:33:00 +0530

    chore: remove stale duplicate Cloudflare env typing

--- src/env.d.ts
+++ src/env.d.ts
@@ -13,13 +13,3 @@ interface ImportMetaEnv {
 interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
-
-declare namespace Cloudflare {
-  interface Env {
-    TURNSTILE_SECRET_KEY?: string;
-    LEAD_DELIVERY_WEBHOOK_URL?: string;
-    LEAD_DELIVERY_TOKEN?: string;
-    SANITY_API_READ_TOKEN?: string;
-    ASSETS: Fetcher;
-  }
-}
